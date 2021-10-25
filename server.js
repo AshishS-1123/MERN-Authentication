73ad5900170098bfd1b50df5ea24e3bf5771e1b9
Ashish Shevale <shevaleashish@gmail.com>
2021-10-25 21:58:10 +0530

    Update index

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express ()
 
 const PORT = process.env.PORT || 5000
 
+app.use (express.json ())
+
+app.use ('/api/auth', require ('./routes/auth'))
+
 app.listen (PORT, () => {
   console.log(PORT);
+})
+
+app.get ('/', (req, res) => {
+  console.log("hey there")
+  res.end ("hello")
 })
