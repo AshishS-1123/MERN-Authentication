5ec0c0bd45f7e86c593a60fa1f948060e73e24d5
Ashish Shevale <shevaleashish@gmail.com>
2021-10-25 23:36:13 +0530

    Schema and hashing for user

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require ('mongoose')
+const bcrypt = require ('bcryptjs')
+
+// The schema for database table
+const UserSchema = new mongoose.Schema ({
+  userName: {
+    type: String,
+    required: [true, "Please provide a user name."]
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email."],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+      "Please provide a valid email."
+    ]
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password."],
+    minlength: 6,
+    select: false // If we query a user we dont want their password unless we ask for it explicitly
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date
+})
+
+// pre('save') will run our callback function before every save operation to database
+// the callback has to be made using function keyword
+UserSchema.pre ('save', async function (next) {
+  if (!this.isModified('password')) {
+    // if password did not change, move to next middleware
+    next ()
+  }
+
+  // salt creates random hash
+  const salt = await bcrypt.genSalt (10)
+  // hash the password and update it
+  this.password = await bcrypt.hash (this.password, salt)
+  next ()
+})
+
+const User = mongoose.model ('User', UserSchema)
+
+module.exports = User
