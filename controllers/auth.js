21e23ff4ae342d8edf6b9b3a09edd268a66a5ff3
Ashish Shevale <shevaleashish@gmail.com>
2021-10-25 23:35:55 +0530

    Added method to register user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,28 @@
-exports.registerUser = (req, res, next) => {
-  res.send ("register route");
+const User = require ('../models/User')
+
+exports.registerUser = async (req, res, next) => {
+  // Get the login info from the request
+  const { userName, email, password } = req.body
+
+  try {
+    // Create a new user in the database.
+    // No need to perform hashing. Will be done using middleware
+    const user = await User.create ({
+      userName, email, password
+    })
+
+    // If everything was done properly, send success to user
+    res.status (201).json ({
+      success: true,
+      user: user
+    })
+  } catch (error) {
+    // In case of error, inform user
+    res.status (500).json ({
+      success: false,
+      error: error.message
+    })
+  }
 }
 
 exports.loginUser = (req, res, next) => {
