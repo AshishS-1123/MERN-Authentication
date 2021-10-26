428bbd7758c1b77cc2b87bb55b6f6dca66b02746
Ashish Shevale <shevaleashish@gmail.com>
2021-10-26 21:33:34 +0530

    Create basic sign in page component

---
+++ client/app/containers/SignInPage/index.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * SignInPage
+ *
+ */
+
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+export function SignInPage() {
+  return <div />;
+}
+
+SignInPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  null,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(SignInPage);
