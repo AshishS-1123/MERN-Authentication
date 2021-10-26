f942cf809f7d444dfb3b198515e51db3b0bc1035
Ashish Shevale <shevaleashish@gmail.com>
2021-10-26 21:27:05 +0530

    Create basic sign in component

---
+++ client/app/components/SignIn/index.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function SignIn() {
+  return <div />;
+}
+
+SignIn.propTypes = {};
+
+export default memo(SignIn);
