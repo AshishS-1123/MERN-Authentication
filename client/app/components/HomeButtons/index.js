4ef1641c7ab1066c6e818109ab640654c6437040
Ashish Shevale <shevaleashish@gmail.com>
2021-10-26 21:33:09 +0530

    Create basic component for home buttons

---
+++ client/app/components/HomeButtons/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * HomeButtons
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function HomeButtons() {
+  return <div />;
+}
+
+HomeButtons.propTypes = {};
+
+export default memo(HomeButtons);
