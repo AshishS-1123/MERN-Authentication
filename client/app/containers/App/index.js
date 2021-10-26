17074b7b266d57c516e6c6efd3c3ee7fc05c2a34
Ashish Shevale <shevaleashish@gmail.com>
2021-10-26 21:34:04 +0530

    Bring everything together

--- client/app/containers/App/index.js
+++ client/app/containers/App/index.js
@@ -1,17 +1,10 @@
-/**
- *
- * App.js
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- *
- */
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
-import HomePage from 'containers/HomePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import HomePage from 'containers/HomePage/Loadable'
+import SignIn from 'containers/SignIn/Loadable'
+import SignUp from 'containers/SignUp/Loadable'
+import NotFoundPage from 'containers/NotFoundPage/Loadable'
 
 import GlobalStyle from '../../global-styles';
 
@@ -20,6 +13,8 @@ export default function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/signIn" component={SignIn} />
+        <Route exact path="/signUp" component={SignUp} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
