3135fb1c5c7d3d6fb65d406e59738c238cd998f9
Ashish Shevale <shevaleashish@gmail.com>
2021-10-28 16:17:46 +0530

    Created basic ForgotPassword password page

---
+++ client/app/containers/ForgotPasswordPage/index.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * ForgotPasswordPage
+ *
+ */
+
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+export function ForgotPasswordPage() {
+  return <div />;
+}
+
+ForgotPasswordPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  null,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(ForgotPasswordPage);
