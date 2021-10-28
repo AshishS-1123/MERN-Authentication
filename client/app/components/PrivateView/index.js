8be0f9c19b3630cdab319eed5eb6807f009f2907
Ashish Shevale <shevaleashish@gmail.com>
2021-10-28 16:39:58 +0530

    Create empty PrevateView component

---
+++ client/app/components/PrivateView/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * PrivateView
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function PrivateView() {
+  return <div />;
+}
+
+PrivateView.propTypes = {};
+
+export default memo(PrivateView);
