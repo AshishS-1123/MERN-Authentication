bbafc77f4e621f137364bf0ee08aed29765ab8c5
Ashish Shevale <shevaleashish@gmail.com>
2021-10-28 16:38:33 +0530

    Create ResetPassword component

--- client/app/components/ResetPassword/index.js
+++ client/app/components/ResetPassword/index.js
@@ -1,17 +1,32 @@
-/**
- *
- * ResetPassword
- *
- */
+import React, { memo } from 'react'
+import { Link } from 'react-router-dom'
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import Input from 'components/Input/Loadable'
+import './index.css'
 
-function ResetPassword() {
-  return <div />;
+function ForgotPassword() {
+  return (
+    <div className='container'>
+      <h1 className="container_heading">
+        <span className="back_button">
+          <Link to='/'>
+            &lt;
+          </Link>
+        </span>
+        Reset Password
+      </h1>
+
+
+      <form className="form">
+        <Input label="Password" type="text" placeholder="Enter Your New Password" autocomplete="password"/>
+        <Input label="Retype Password" type="text" placeholder="Enter Your Password Again" autocomplete="password"/>
+
+        <button type='submit' className='button'>Update Password</button>
+      </form>
+    </div>
+  )
 }
 
-ResetPassword.propTypes = {};
+ForgotPassword.propTypes = {};
 
-export default memo(ResetPassword);
+export default memo(ForgotPassword);
