d50e6c6f9db387756e16d1274ddf66bea11e1c43
Ashish Shevale <shevaleashish@gmail.com>
2021-10-28 16:17:14 +0530

    Created basic ForgotPassword component

---
+++ client/app/components/ForgotPassword/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * ForgotPassword
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function ForgotPassword() {
+  return <div />;
+}
+
+ForgotPassword.propTypes = {};
+
+export default memo(ForgotPassword);
