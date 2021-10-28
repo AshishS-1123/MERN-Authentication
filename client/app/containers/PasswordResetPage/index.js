0722cdc7987cf2647264ada40a404aba9d33c13e
Ashish Shevale <shevaleashish@gmail.com>
2021-10-28 16:18:08 +0530

    Created basic Reset Password password page

---
+++ client/app/containers/PasswordResetPage/index.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * PasswordResetPage
+ *
+ */
+
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+export function PasswordResetPage() {
+  return <div />;
+}
+
+PasswordResetPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  null,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(PasswordResetPage);
