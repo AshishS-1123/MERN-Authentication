92854098bc5f50b1a5029caeb1c55e6272f449ed
Ashish Shevale <shevaleashish@gmail.com>
2021-10-28 16:17:27 +0530

    Created basic ResetPassword component

---
+++ client/app/components/ResetPassword/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * ResetPassword
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function ResetPassword() {
+  return <div />;
+}
+
+ResetPassword.propTypes = {};
+
+export default memo(ResetPassword);
